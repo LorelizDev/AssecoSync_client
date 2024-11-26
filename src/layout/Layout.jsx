4be4bc8895176e94f6d636331daa4d34d8851261
devegalaura-dev <lauradevega94@gmail.com>
2024-11-26 12:05:22 +0100

    Add authStore, Layout and PrivateRoutes

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,1 +1,13 @@
-//layout
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import { Navbar } from '../components/Navbar';
+import { Footer } from '../components/Footer';
+export const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
