11c432a23960dba3e5c4a15cf0dbbc9432a94f64
devegalaura-dev <lauradevega94@gmail.com>
2024-11-26 15:14:10 +0100

    Add Routes; generals, private for admins, and private for employees

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,1 +1,127 @@
-//router
+// import { Layout } from '../layout/Layout.jsx';
+// import { EmployeeRoutes, AdminRoutes } from '../layout/PrivateRoutes.jsx';
+// import {
+//     Login,
+//     SignUp,
+//     Home,
+//     Profile,
+//     PersonalCalendar,
+//     MyRequests,
+//     Certificate,
+//     Request,
+//     RequestsList,
+//     Register,
+//     EmployeesList,
+//     AdminCalendar,
+//     Stadistics
+// } from '../pages';
+
+// export const router = createBrowserRouter([{
+//     path : '/',
+//     element: <Layout/>,
+//     children: [
+//             {
+//                 path:'login',
+//                 element: <Login/>
+//             },
+//             {
+//             path : 'auth',
+//             element: <AuthRoutes/>,
+//             children: [
+//                 {
+//                     index: true,
+//                     path: 'home',
+//                     element: <Home/>
+//                 },
+//                 {
+//                     path : 'employee',
+//                     element: <EmployeeRoutes/>,
+//                     children: [
+//                             {
+//                                 path:'profile',
+//                                 element: <Profile/>,
+//                                 children: [
+//                                     {
+//                                         path:'absences',
+//                                         element: <AbsencesList/>,
+//                                         children: [
+//                                             {
+//                                                 path:'my_certificate',
+//                                                 element: <Certificate/>
+//                                             },
+//                                         ]
+//                                     },
+//                                     {
+//                                         path:'clock_in_list',
+//                                         element: <ClockInList/>
+//                                     },
+//                                 ]
+//                             },
+//                             {
+//                                 path:'personal_calendar',
+//                                 element: <PersonalCalendar/>,
+//                                 children: [
+//                                     {
+//                                         path:'request',
+//                                         element: <Request/>
+//                                     },
+//                                 ]
+//                             },
+//                             {
+//                                 path:'my_requests',
+//                                 element: <MyRequests/>
+//                             },
+//                     ]
+//                 },
+//                 {
+//                     path : 'admin',
+//                     element: <AdminRoutes/>,
+//                     children: [
+//                         {
+//                             path:'requests_list',
+//                             element: <RequestsList/>
+//                         },
+//                         {
+//                             path:'register_new_employee',
+//                             element: <Register/>
+//                         },
+//                         {
+//                             path:'employee_profile',
+//                             element: <Profile/>,
+//                             children: [
+//                                 {
+//                                     path:'absences',
+//                                     element: <AbsencesList/>,
+//                                     children: [
+//                                         {
+//                                             path:'certificate_absence',
+//                                             element: <Certificate/>
+//                                         },
+//                                         {
+//                                             path:'edit_absence',
+//                                             element: <EditAbsence/>
+//                                         },
+//                                     ]
+//                                 },
+//                                 {
+//                                     path:'clock_in_list',
+//                                     element: <ClockInList/>,
+//                                     children: [
+//                                         {
+//                                             path:'edit_clock_in',
+//                                             element: <EditClockIn/>
+//                                         },
+//                                     ]
+//                                 },
+//                                 {
+//                                     path:'edit_profile',
+//                                     element: <EditProfile/>
+//                                 },
+//                             ],
+//                         },
+//                     ],
+//                     },
+//             ],
+//             },
+//         ],
+// }])
